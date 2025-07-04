922f00e99b6ae530345cac87fa1296a0e4525b14
Jin JunWoo <53937347+wlswnsdn@users.noreply.github.com>
2025-07-04 21:07:59 +0900

    Feat/#1/ci cd 세팅 (#3)
    * feat: 프로젝트 초기 세팅
    
    * feat: Dockerfile 생성
    
    * feat: CI.yml, CD.yml 작성
    
    * feat: S3 버킷 이름, 리전 환경 변수 추가
    
    * chore: S3, .env 관련 의존성 추가
    
    * feat: db, s3 .yml 파일 추가
    
    * feat: S3Config 추가
    
    * docs: PR 탬플릿 추가
    
    * docs: swagger 추가
    
    ---------
    
    Co-authored-by: wlswnsdn <wlswnsdn123@gmail.com>

---
+++ src/main/java/channeling/be/global/config/S3Config.java
@@ -0,0 +1,29 @@
+package channeling.be.global.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+public class S3Config {
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucketName;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .region(Region.of(region))
+                .build();
+    }
+
+    @Bean
+    public String getBucketName() {
+        return bucketName;
+    }
+}
