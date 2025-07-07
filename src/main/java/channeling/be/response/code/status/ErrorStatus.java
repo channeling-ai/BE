a2cd103ea272cbcca0638ac05ed4cc05b7d49906
Miging <dnfldpden32@gmail.com>
2025-07-07 22:08:57 +0900

    feat: 에러 코드 및 핸들러 정의

--- src/main/java/channeling/be/response/code/status/ErrorStatus.java
+++ src/main/java/channeling/be/response/code/status/ErrorStatus.java
@@ -15,8 +15,12 @@ public enum ErrorStatus implements BaseErrorCode {
     _INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"COMMON500", "서버 에러, 관리자에게 문의 바랍니다."),
     _BAD_REQUEST(HttpStatus.BAD_REQUEST, "COMMON400", "잘못된 요청입니다."),
     _UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "COMMON401", "인증이 필요합니다."),
-    _FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403","금지된 요청입니다.");
+    _FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403","금지된 요청입니다."),
 
+
+    //채널 관련 에러
+    _CHANNEL_NOT_FOUND(HttpStatus.NOT_FOUND, "CHANNEL404", "존재하지 않는 채널입니다."),
+    _CHANNEL_NOT_MEMBER(HttpStatus.FORBIDDEN, "CHANNEL403", "해당 채널을 소유한 멤버가 아닙니다.");
 
 
 
