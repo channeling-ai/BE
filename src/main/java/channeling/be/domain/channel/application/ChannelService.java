aa29e20c59efee51f54c583a89efdcf21be2694f
Miging <dnfldpden32@gmail.com>
2025-07-07 22:09:51 +0900

    feat: 채널 서비스 로직 설계

--- src/main/java/channeling/be/domain/channel/application/ChannelService.java
+++ src/main/java/channeling/be/domain/channel/application/ChannelService.java
@@ -1,4 +1,14 @@
 package channeling.be.domain.channel.application;
 
+import channeling.be.domain.channel.presentation.ChannelResDTO;
+import channeling.be.response.exception.GeneralException;
+
 public interface ChannelService {
+	/**
+	 * 채널 ID로 채널의 존재 여부를 검증합니다.
+	 *
+	 * @param channelId 채널 ID
+	 * @throws channeling.be.response.exception.handler.ChannelHandler 채널이 존재하지 않을 경우 예외 발생
+	 */
+	void validateChannelByIdAndMember(Long channelId);
 }
--- src/main/java/channeling/be/domain/channel/application/ChannelServiceImpl.java
+++ src/main/java/channeling/be/domain/channel/application/ChannelServiceImpl.java
@@ -1,5 +1,9 @@
 package channeling.be.domain.channel.application;
 
+import channeling.be.domain.channel.domain.repository.ChannelRepository;
+import channeling.be.domain.channel.presentation.ChannelResDTO;
+import channeling.be.response.code.status.ErrorStatus;
+import channeling.be.response.exception.handler.ChannelHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,4 +12,15 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 @Service
 public class ChannelServiceImpl implements ChannelService {
+
+	private final ChannelRepository channelRepository;
+
+	@Override
+	public void validateChannelByIdAndMember(Long channelId) {
+		boolean isExist=channelRepository.existsById(channelId);
+		if (!isExist) {
+			throw new ChannelHandler(ErrorStatus._CHANNEL_NOT_FOUND);
+		}
+		//TODO: 추후 유저 + 채널 연관 관계 확인 로직 필요
+	}
 }
