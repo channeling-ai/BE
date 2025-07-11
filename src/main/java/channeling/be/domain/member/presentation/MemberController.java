fc2a6dec90de22f33d52be3e55fa9d17ddffa54e
Miging <dnfldpden32@gmail.com>
2025-07-11 15:29:18 +0900

    chore: HTTP 메소드 변경 (Post->Patch)

--- src/main/java/channeling/be/domain/member/presentation/MemberController.java
+++ src/main/java/channeling/be/domain/member/presentation/MemberController.java
@@ -9,6 +9,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,7 +29,7 @@ public class MemberController {
 	 * 멤버의 sns 정보를 수정하는 API입니다.
 	 * @return 멤버의 기본 정보
 	 */
-	@PostMapping("/update-sns")
+	@PatchMapping("/update-sns")
 	@Operation(
 		summary = "SNS 정보 수정 API",
 		description = "멤버의 SNS 정보를 수정합니다. 현재는 임시로 1L로 설정된 멤버 ID를 사용합니다."
