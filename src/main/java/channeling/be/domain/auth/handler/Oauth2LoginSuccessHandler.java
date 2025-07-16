e393579623dbfa8cb4c53e7b1e0f8633eb75b4fe
Miging <dnfldpden32@gmail.com>
2025-07-16 19:17:21 +0900

    feat: 채널 정보, 영상 조회 인터페이스 적용

--- src/main/java/channeling/be/domain/auth/handler/Oauth2LoginSuccessHandler.java
+++ src/main/java/channeling/be/domain/auth/handler/Oauth2LoginSuccessHandler.java
@@ -1,9 +1,12 @@
 package channeling.be.domain.auth.handler;
 
+import channeling.be.domain.channel.application.ChannelService;
+import channeling.be.domain.channel.domain.Channel;
+import channeling.be.domain.member.application.MemberService;
 import channeling.be.domain.member.domain.Member;
-import channeling.be.domain.member.domain.repository.MemberRepository;
 import channeling.be.global.infrastructure.jwt.JwtUtil;
 import channeling.be.global.infrastructure.redis.RedisUtil;
+import channeling.be.global.infrastructure.youtube.YoutubeUtil;
 import channeling.be.response.exception.handler.ApiResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
@@ -33,7 +36,8 @@ public class Oauth2LoginSuccessHandler implements AuthenticationSuccessHandler {
     private final OAuth2AuthorizedClientService authorizedClientService;
     private final JwtUtil jwtUtil;    // JWT 토큰 생성기
     private final RedisUtil redisUtil;
-    private final MemberRepository memberRepository;
+    private final MemberService memberService;
+    private final ChannelService channelService;
 
     // 로그인 성공 시 처리하는 메서드
     @Override
@@ -59,15 +63,29 @@ public class Oauth2LoginSuccessHandler implements AuthenticationSuccessHandler {
         Map<String, Object> attrs = oauthUser.getAttributes(); // 멤버 속성
         log.info("컨텍스트에서 토큰 내의 회원 정보 추출 = {}" , attrs);
 
-        // TODO DB 조회해서, 실제 존재하는 사람인가? -> 있으면 기존에 있던 사람 -> 통과! + 멤버 반환, 없으면 -> 구글로그인 성공한 처음 요청 -> 회원가입 진행
-//        Member member = memberService.findOrCreateMember(attrs.get()); attr에서 구글 아이디로 조회해야 할 듯...?
-        Member member =  memberRepository.findByGoogleId(attrs.get("sub").toString()).get(); // 나중에 삭제해야함
+
+        /* -------------------------------------------------
+         * 1. 멤버 생성/조회
+         * 2. 채널 생성/조회
+         * 3. 유튜브에서 채널정보 조회
+         * 4. 유튜브에서 채널 분석 정보 조회
+         * 5. 유튜브에서 비디오 정보 조회
+         * ------------------------------------------------- */
+        Member member = memberService.findOrCreateMember(
+            attrs.get("sub").toString(), // 구글 아이디
+            attrs.get("email").toString(), // 구글 이메일
+            attrs.get("name").toString()
+        );
 
         log.info("회원 구글 아이디로 db 조회 = {}" , member.getNickname());
 
         //멤버 아이디를 키값으로 해서 redis 에 구글 엑세스 토큰 저장
         redisUtil.saveGoogleAccessToken(member.getId(), googleAccessToken);
         log.info("멤버 아이디를 키값으로 해서 redis 에 구글 엑세스 토큰 저장 ");
+
+        Channel channel = channelService.findOrCreateChannelByMember(member);
+
+        log.info("회원 아이디로 db 채널 조회 = {}" , channel.getId());
 
 
         // jwt 생성 -> 우리 서버 자체 access 토큰
