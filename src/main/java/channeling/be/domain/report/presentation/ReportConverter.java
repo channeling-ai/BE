d2bcea1c8f1f0c5ef1757dc670d3118636239ba2
chang min O <82094699+Ochangmin524@users.noreply.github.com>
2025-08-06 00:38:48 +0900

    [CHORE] 타입별 댓글 목록 조회 서비스, 컨버터단 구현 완료

--- src/main/java/channeling/be/domain/report/presentation/ReportConverter.java
+++ src/main/java/channeling/be/domain/report/presentation/ReportConverter.java
@@ -1,7 +1,12 @@
 package channeling.be.domain.report.presentation;
 
+import channeling.be.domain.comment.domain.Comment;
+import channeling.be.domain.comment.domain.CommentType;
 import channeling.be.domain.report.domain.Report;
 import channeling.be.domain.task.domain.Task;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ReportConverter {
     public static ReportResDto.getReportAnalysisStatus toReportAnalysisStatus(Task task, Report report) {
@@ -11,5 +16,18 @@ public class ReportConverter {
                 task.getOverviewStatus(),
                 task.getAnalysisStatus(),
                 task.getIdeaStatus());
+    }
+
+    public static ReportResDto.getCommentsByType toCommentsByType(CommentType commentType, List<Comment> commentList) {
+        return new ReportResDto.getCommentsByType(
+                commentType,
+                commentList.stream()
+                        .map(ReportConverter::toSingleCommentRes)
+                        .collect(Collectors.toList())
+        );
+    }
+
+    private static ReportResDto.SingleCommentRes toSingleCommentRes(Comment comment) {
+        return new ReportResDto.SingleCommentRes(comment.getCommentType(), comment.getId(), comment.getContent());
     }
 }
