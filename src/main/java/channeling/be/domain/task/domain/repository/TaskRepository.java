e5391ab3f62c1761112e03fe9023ecdb4818dd0b
wlswnsdn <wlswnsdn123@gmail.com>
2025-08-11 00:44:37 +0900

    [FIX] 필요없는 task 레포 메서드 삭제

--- src/main/java/channeling/be/domain/task/domain/repository/TaskRepository.java
+++ src/main/java/channeling/be/domain/task/domain/repository/TaskRepository.java
@@ -22,8 +22,4 @@ public interface TaskRepository extends JpaRepository<Task, Long> {
     Optional<Task> findByReportId(Long reportId);
 
     void deleteTaskByReportId(Long id);
-
-    @Query(value = "SELECT * FROM task WHERE id = :taskId", nativeQuery = true)
-    Optional<Task> findByIdNative(@Param("taskId") Long taskId);
-
 }
