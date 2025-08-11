992039adb0518b8afbafa2758887bebb906ebf5f
chang min O <82094699+Ochangmin524@users.noreply.github.com>
2025-08-12 02:41:44 +0900

    [CHORE] 멤버 동의 수정에 응답에 dto 추가 및 reqDto private 삭제로 valid 적용 되도록 수정

--- src/main/java/channeling/be/domain/memberAgree/presentation/MemberAgreeReqDto.java
+++ src/main/java/channeling/be/domain/memberAgree/presentation/MemberAgreeReqDto.java
@@ -1,16 +1,16 @@
 package channeling.be.domain.memberAgree.presentation;
 
 import jakarta.validation.constraints.NotNull;
-import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 
 public class MemberAgreeReqDto {
 
-    @Data
+    @Getter
     public static class Edit {
-        @NotNull
-        private Boolean marketingEmailAgree;
-        @NotNull
-        private Boolean dayContentEmailAgree;
+        @NotNull(message = "null 이어서는 안됩니다..")
+        Boolean marketingEmailAgree;
+
+        @NotNull(message = "null 이어서는 안됩니다..")
+        Boolean dayContentEmailAgree;
     }
 }
