ed65287fb3bb733eb493188efd432795f78deeaa
heo <=>
2025-10-28 17:07:49 +0900

    [FIX] 최초생성자, 최종생성자 설정 오류 수정

--- src/main/java/channeling/be/global/config/AuditorAwareConfig.java
+++ src/main/java/channeling/be/global/config/AuditorAwareConfig.java
@@ -5,16 +5,27 @@ import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 
 import java.util.Optional;
 
-public class AuditorAwareConfig implements AuditorAware<CustomUserDetails> {
+public class AuditorAwareConfig implements AuditorAware<String> {
     @Override
-    public Optional<CustomUserDetails> getCurrentAuditor() {
+    public Optional<String> getCurrentAuditor() {
         return Optional.ofNullable(SecurityContextHolder.getContext())
                 .map(SecurityContext::getAuthentication)
                 .filter(Authentication::isAuthenticated)
                 .map(Authentication::getPrincipal)
-                .map(CustomUserDetails.class::cast);
+                .map(principal -> {
+                    if (principal instanceof CustomUserDetails customUser) {
+                        return customUser.getMember().getGoogleEmail(); // JWT 유저네임
+                    } else if (principal instanceof DefaultOAuth2User oAuth2User) {
+                        Object emailAttr = oAuth2User.getAttribute("email");
+                        if (emailAttr instanceof String) {
+                            return (String) emailAttr; // 구글 이메일
+                        }
+                    }
+                    return null;
+                });
     }
 }
